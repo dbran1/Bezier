fa9365c0bde89cd62766dc3938d66731bb580779
dbran1 <83590611+dbran1@users.noreply.github.com>
2023-01-16 11:20:19 -0500

    Update particle.js

--- particle.js
+++ particle.js
@@ -7,8 +7,13 @@ class Particle {
   }
   
    update() {
+     let paused = false;
+     let dx2;
+     let dy2;
+    if(keyIsPressed===false){
     this.x += this.dx;
     this.y += this.dy;
+    }  
     
     if(this.x>=width||this.x<0) {
       this.dx *= -1;
@@ -16,6 +21,28 @@ class Particle {
      
     if(this.y>=height||this.y<0) {
       this.dy *= -1;
+    }
+     
+    if(keyIsDown(32)&&paused===false){{
+      dx2=this.dx;
+      dy2= this.dy
+      this.dx = 0;
+      this.dy = 0;
+      paused=true;
+    }
+   if(keyIsDown(32)&&paused===true){
+     this.dx=dx2
+     this.dy=dy2
+     paused=false
+   }
+   }
+    if(keyIsDown(UP_ARROW)){
+      this.dx+=1
+      this.dy+=1
+    }
+    else if(keyIsDown(DOWN_ARROW)){
+      this.dx/=2
+      this.dy/=2
     }
   }
 }
